e79c5e5eecc6d8ffd9f6e00be1b30ddd8f654293
Hanna <westlund90@hotmail.com>
2020-02-27 12:59:13 +0100

    not finished

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express')
+const Datastore = require('nedb')
 const app = express()
-const session = require('express-session')
-
-const fs = require('fs')
-
+const insults = new Datastore({filename:'insults.db', autoload: true})
 
 app.use( express.json() )
 
-app.use(session({
-    secret: "thisisweakkey", //key for encrypting cookies
-    cookie: {},
-    saveUninitialized: false,
-    resave: false
-}))
-
 app.get('/Hello', (req, res) => {
     res.send("Hello world")
+})
+
+app.get('/insults/:severity', async (req, res) => {
+    res.set("Content-Type", "application/json")
+    let result = await insults.find({ severity: parseInt(req.params.severity) }, function (err, docs) {
+        res.json({"insults": result})
+    });
 })
 
 app.listen(8080, () => console.log("Server started"))
